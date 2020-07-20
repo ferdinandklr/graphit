760a78267410aeac6b1db22aff8cfd3dbd7ff44e
Ferdinand Keller <ferdinandklr@gmail.com>
2020-07-20 19:08:22 +0200

    :lipstick: themes & style

--- src/App.vue
+++ src/App.vue
@@ -9,3 +9,11 @@ export default {
   name: 'App'
 }
 </script>
+
+<style lang="sass">
+// import global sass
+@import "./assets/typo/typo.css"
+@import "./assets/styling/palette.sass"
+@import "./assets/styling/general.sass"
+@import "./assets/styling/themes.sass"
+</style>
