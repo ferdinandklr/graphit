e8ae5346802a39a1207b93c3132e6bec7092b90f
Ferdinand Keller <ferdinandklr@gmail.com>
2020-07-21 22:55:52 +0200

    :ok_hand: put the window border in its own file

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
 
     <!-- code related to windows -->
     <titlebar />
-    <div class="windowBorder"></div>
+    <window-border />
 
   </div>
 </template>
@@ -20,11 +20,13 @@ import { theme } from '@/stores/AppState'
 
 // import components
 import Titlebar from '@/components/Titlebar'
+import WindowBorder from '@/components/WindowBorder'
 
 export default {
   name: 'App',
   components: {
-    titlebar: Titlebar
+    titlebar: Titlebar,
+    'window-border': WindowBorder
   },
   setup() {
     return {
@@ -42,13 +44,4 @@ export default {
 @import "./assets/styling/general.sass"
 
 #app
-  .windowBorder
-    width: 100%
-    height: 100%
-    position: absolute
-    top: 0
-    left: 0
-    z-index: 100
-    pointer-events: none
-    border: solid 1px var(--window-border-color)
 </style>
---
+++ src/components/WindowBorder.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="window-border"></div>
+</template>
+
+<script>
+export default {
+  name: 'WindowBorder'
+}
+</script>
+
+<style lang="sass">
+.window-border
+  width: 100%
+  height: 100%
+  position: absolute
+  top: 0
+  left: 0
+  z-index: 100
+  pointer-events: none
+  border: solid 1px var(--window-border-color)
+</style>
