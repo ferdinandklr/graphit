e392953eef03a6cdd322e42b7a9710cfabb06a8a
Ferdinand Keller <ferdinandklr@gmail.com>
2020-07-22 18:15:49 +0200

    :sparkles: add views tables and plot2d files

---
+++ src/views/Plot2D.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="plot2d"></div>
+</template>
+
+<script>
+export default {
+  name: 'Plot2D'
+}
+</script>
+
+<style lang="sass">
+.plot2d
+</style>
---
+++ src/views/Tables.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="tables"></div>
+</template>
+
+<script>
+export default {
+  name: 'Tables'
+}
+</script>
+
+<style lang="sass">
+.plot2d
+</style>
