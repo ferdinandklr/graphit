fa3008ad0570a0948a046a7793e1b4210f911b15
Ferdinand Keller <ferdinandklr@gmail.com>
2020-07-22 15:46:41 +0200

    :sparkles: add a file for all constants

---
+++ src/constants.js
@@ -0,0 +1,26 @@
+import { readonly } from 'vue'
+
+/* ---- THEMES ---- */
+const THEMES = readonly({
+  DARK: 'DARK',
+  LIGHT: 'LIGHT'
+})
+
+/* ---- SIDEBAR ---- */
+// subs
+const SIDEBAR_SUBS = readonly({
+  MENU: 'MENU',
+  CREATETABLE: 'CREATETABLE'
+})
+
+// menu
+const MENUSUB_VIEWS = readonly({
+  TABLES: 'TABLES',
+  PLOT2D: 'PLOT2D',
+  PLOT3D: 'PLOT3D',
+  FOURRIER: 'FOURRIER',
+  CHARTS: 'CHARTS'
+})
+
+/* ---- EXPORT ---- */
+export { THEMES, SIDEBAR_SUBS, MENUSUB_VIEWS }
